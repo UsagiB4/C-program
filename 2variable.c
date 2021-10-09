7bcddb3597c228af25765bed7c6495cfbf75ca67
UsagiB4 <binaryBunny@tutanota.com>
2021-10-09 12:21:48 +0600

    Variables in C

---
+++ 2variable.c
@@ -0,0 +1,15 @@
+# include <stdio.h>
+/*
+this is a multiple line comment.
+*/
+
+int main(){
+    int a_a = 12;  // This is an intiger
+    char b = 'h';  // This is a character
+    float _fl = 23.2;  // This is a floating number
+    int aNum;
+    printf("%d is not equal to %f \n", a_a, _fl);
+    printf("%cello world", b);
+
+    return 0;
+}
