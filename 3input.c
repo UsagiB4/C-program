67b266dffe2d66bb546eb7932deecfd60b6336e6
UsagiB4 <binaryBunny@tutanota.com>
2021-10-11 19:09:15 +0600

    Input

---
+++ 3input.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int a, b, c;
+    printf("Enter the value of a: \n");
+    scanf("%d", &a);
+    printf("Enter the value of b: \n");
+    scanf("%d", &b);
+    c = a + b;
+    printf("%d + %d = %d", a, b, c);
+    return 0;
+}
