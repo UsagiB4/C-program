e15b8e13e09af1327c93fadbe0002d359a520c98
UsagiB4 <binaryBunny@tutanota.com>
2021-10-25 15:53:52 +0600

    Loop in C

---
+++ 4loop.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/*
+Loops in C lang.
+> while
+> do while
+> for
+*/
+
+int main(){
+    int a;
+    scanf("%d", &a);
+    while(a < 10)
+    {
+        printf("%d \n", &a);
+        a++;
+    }
+    
+    return 0;
+}
+
