0f0924ad74a9d1bd80ee615c41f4f91f8266ee06
UsagiB4 <binaryBunny@tutanota.com>
2021-12-03 02:27:37 +0600

    Variables

--- 2variable.c
+++ 2variable.c
@@ -6,8 +6,10 @@ this is a multiple line comment.
 int main(){
     int a_a = 12;  // This is an intiger
     char b = 'h';  // This is a character
+    char name[] = "Mark"; // This is an array
     float _fl = 23.2;  // This is a floating number
     int aNum;
+    printf("Hello %s \n", name);
     printf("%d is not equal to %f \n", a_a, _fl);
     printf("%cello world", b);
 
