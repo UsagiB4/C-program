1f5eda39d0d7faf7d931e7f792794510b367f8e0
UsagiB4 <binaryBunny@tutanota.com>
2021-12-06 03:56:10 +0600

    Taking input

--- 3input.c
+++ 3input.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 int main()
 {
@@ -14,6 +15,7 @@ int main()
     printf("%d + %d = %d \n", a, b, c);
 
     /* String input */
+    printf("Enter your name: ");
     char name[30];
     scanf("%s", name);
     printf("Hello %s", name);
