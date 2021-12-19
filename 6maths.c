b09ea9b74f8a994c348ee12360081a8b61f467f6
UsagiB4 <binaryBunny@tutanota.com>
2021-12-20 01:37:31 +0600

    Some math function

---
+++ 6maths.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    double A = sqrt(16); // this will return square root for 16
+    int B = pow(2, 3);  // this will return 2^3 = 8
+    int c = round(1.2233);
+    int d = ceil(23.1);
+    int x = floor(3.23423);
+    int abs = fabs(-323);  // this will return the absolute value
+    double num1 = log(3);  // log of 3
+    printf("%f", num1);
+    return 0;
+}
