950b6fd50cf9fc636e151acf9a7abaa83bdc1e7b
UsagiB4 <binaryBunny@tutanota.com>
2021-12-23 20:26:42 +0600

    Logical Ops

---
+++ 8logics.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+    /*
+                  And
+     ______________________________________
+     |logical operator for AND is `&&`    |
+     |Checks 2 or more condition is true. | 
+     |____________________________________|
+     */
+
+}
