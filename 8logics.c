6802964f78e3433e5dd8f8b289979b8c771f3ec2
UsagiB4 <binaryBunny@tutanota.com>
2021-12-23 20:33:16 +0600

    Logical Ops

--- 8logics.c
+++ 8logics.c
@@ -8,5 +8,18 @@ int main(){
      |Checks 2 or more condition is true. | 
      |____________________________________|
      */
+    //_____code_____
+    int price;
+    printf("Enter the price of eggs\n");
+    scanf("%d", &price);
+    if(price >0 && price <= 40){
+        printf("I can buy eggs! :D ");
+    }
+    else if (price > 40){
+        printf("Too expensive for me! :( ");
+    }
+    else{
+        printf("Invalid Value");
+    }
 
 }
