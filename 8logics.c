9295667036a3101af725204b52c701cca33f325d
UsagiB4 <binaryBunny@tutanota.com>
2021-12-25 18:53:09 +0600

    AND

--- 8logics.c
+++ 8logics.c
@@ -11,7 +11,6 @@ int main(){
      */
     //_____code_____
 
-    /*
     int price;
     printf("Enter the price of eggs\n");
     scanf("%d", &price);
@@ -24,47 +23,9 @@ int main(){
     else{
         printf("Invalid Value");
     }
-    */
-
-    /*
-                  OR
-     ______________________________________
-     |logical operator for OR is `||`     |
-     |Checks if at least 1 condition      |
-     |is true.                            |  
-     |____________________________________|
-     */
-    //_____code_____
-    int weth;
-    int obj;
-    printf("Enter current weather status:\nSunny (1)\nRainy(2)\nCold(3)\n>>> ");
-    scanf("%d", &weth);
     
-    if(weth == 1){
-        printf("Have a nice day");
-    }
-    else if(weth == 2){
-        printf("What are you carrying with you?\nUmbrella(1)\nRain Coat(2)\n");
-        scanf("%d", &obj);
-        if (obj == 1 || obj == 2){
-        
-            printf("Have fun in the rain");
-        }
-        else{
-            printf("Please take an umbrella with you.");
-        }
-        
-    }
-    else if (weth == 3){
-        printf("What are you wearing today?\nCoat(1)\nSweater(2)\n");
-        scanf("%d", &obj);
-        if(obj == 1 || obj == 2){
-            printf("You are free to go.");
-        }
-        else{
-            printf("Wear something to stay warm");
-        }
-    }
+
+    
     return 0;
 
 }
