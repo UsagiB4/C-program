d08d068e61b519df4177e2cb697a5b1bcb8fb32e
UsagiB4 <binaryBunny@tutanota.com>
2021-12-29 15:47:47 +0600

    How Return works

--- 11return.c
+++ 11return.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
+/*
+in the squ() function we are returning an integer
+called square. 
+as we're returning integer, we've used `int` before 
+creating the function. If we want to return a float/
+double/ char, we have to add it's format specifier. 
+*/
 int squ(int x){
     int square = x * x;
     return square;
